034327d95b0fc3490180b68754ecbb99bf955a35
arafatGithubCode <md489288@gmail.com>
2023-12-03 13:02:35 +0600

    simple matching validation with MVC

---
+++ C-1 with MVC/controllers/user.controller.js
@@ -0,0 +1,38 @@
+const User = require("../models/user.model");
+
+const registerUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json({
+      message: "User is created",
+      data: newUser,
+    });
+  } catch (error) {
+    res.status(401).json({
+      message: "User is not created",
+      error: error.message,
+    });
+  }
+};
+
+const logInUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user && password === password) {
+      res.status(201).json({
+        message: "user is logged-In",
+        data: user,
+      });
+    } else {
+      res.status(401).send("User data is not found");
+    }
+  } catch (error) {
+    res.status(401).json({
+      message: "Bad user credential",
+      error: error.message,
+    });
+  }
+};
+module.exports = { registerUser, logInUser };
